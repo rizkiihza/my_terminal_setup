673e6e53b95957bb586707e73fe7fce67370a4e3
rizkiihza <rizki.ihza@gmail.com>
2018-03-29 23:26:28 +0700

    tambah zshrc

--- tmux.sh
+++
@@ -1,7 +0,0 @@
-tmux split-window -h
-tmux split-window -v
-tmux select-pane -t 0
-tmux resize-pane -R 20
-tmux select-pane -t 2
-tmux resize-pane -U 5
-tmux select-pane -t 0
--- tmux2.sh
+++
@@ -1,7 +0,0 @@
-tmux split-window -h
-tmux split-window -h
-tmux select-pane -t 0
-tmux resize-pane -R 10
-tmux select-pane -t 1
-tmux resize-pane -R 5
-tmux select-pane -t 0
